8eda6061d44245b146baf3a8ad0095084ee56c93
in-th3-l00p <indianucode0303@gmail.com>
2023-12-06 19:31:09 +0200

    fixed password change form

--- resources/views/users/editPassword.blade.php
+++ resources/views/users/editPassword.blade.php
@@ -13,44 +13,48 @@
             <h1 class="col mb-5">Schimba parola</h1>
         </div>
         <div class="row mb-5">
-            <label for="current_password">Parola curenta:</label>
-            <input
-                type="password"
-                name="current_password"
-                id="current_password"
-                class="form-control"
-            >
-            @error("current_password")
-                <p class="text-danger">{{ $message }}</p>
-            @enderror
+            <div class="col">
+                <label for="current_password">Parola curenta:</label>
+                <input
+                    type="password"
+                    name="current_password"
+                    id="current_password"
+                    class="form-control"
+                >
+                @error("current_password")
+                    <p class="text-danger">{{ $message }}</p>
+                @enderror
+            </div>
         </div>
         <div class="row mb-5">
-            <label for="password">Parola noua:</label>
-            <input
-                type="password"
-                name="password"
-                id="password"
-                class="form-control"
-            >
-            @error("password")
-                <p class="text-danger">{{ $message }}</p>
-            @enderror
+            <div class="col">
+                <label for="password">Parola noua:</label>
+                <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    class="form-control"
+                >
+                @error("password")
+                    <p class="text-danger">{{ $message }}</p>
+                @enderror
+            </div>
         </div>
         <div class="row mb-5">
-            <label for="password_confirmation">Confirma parola noua:</label>
-            <input
-                type="password"
-                name="password_confirmation"
-                id="password_confirmation"
-                class="form-control"
-            >
-            @error("password_confirmation")
-                <p class="text-danger">{{ $message }}</p>
-            @enderror
+            <div class="col">
+                <label for="password_confirmation">Confirma parola noua:</label>
+                <input
+                    type="password"
+                    name="password_confirmation"
+                    id="password_confirmation"
+                    class="form-control"
+                >
+                @error("password_confirmation")
+                    <p class="text-danger">{{ $message }}</p>
+                @enderror
+            </div>
         </div>
 
-        <div class="row mb-5">
-            <button class="btn btn-dark mx-auto">Salveaza</button>
-        </div>
+        <button class="btn btn-dark mb-5">Salveaza</button>
     </form>
 @endsection
